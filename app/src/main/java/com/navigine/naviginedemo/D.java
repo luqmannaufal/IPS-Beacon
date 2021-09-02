b080bdb77af1e9c80b67100a6f17466e1086762c
luqmannaufal <luqmannaufal15@gmail.com>
2021-09-02 21:16:05 +0700

    Bismillah Tugas Akhir 2021 IPS-Beacon

---
+++ app/src/main/java/com/navigine/naviginedemo/D.java
@@ -0,0 +1,7 @@
+package com.navigine.naviginedemo;
+public final class D {
+  public static final String  SERVER_URL    = "https://api.navigine.com";
+  public static final String  USER_HASH     = "F860-5D64-BC0A-1B3E";
+  public static final String  LOCATION_NAME = "indomaret kos 43c";
+  public static final boolean WRITE_LOGS    = false;
+}
